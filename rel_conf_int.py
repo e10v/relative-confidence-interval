a241345783a30c35fc8667e8c2bfac9e8f49ac6a
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2022-05-15 00:49:10 +0300

    Change style and refactor

--- rel_conf_int.py
+++ rel_conf_int.py
@@ -1,27 +1,30 @@
 from collections.abc import Callable
+
 import numpy as np
+from numpy.typing import ArrayLike
 import pandas as pd
 from scipy import stats
 from tqdm import tqdm
-from IPython.display import display
 
 
-seed = 42
-true_control_mean = 5
-true_relative_diff = 0.2
-shape = 0.2
-control_size = 2000
-treatment_size = control_size
-n_experiments = 10000
+SEED = 42
+TRUE_CONTROL_MEAN = 5
+TRUE_RELATIVE_DIFF = 0.2
+TRUE_TREATMENT_MEAN = TRUE_CONTROL_MEAN * (1 + TRUE_RELATIVE_DIFF)
 
+CONTROL_SIZE = 2000
+TREATEMT_SIZE = CONTROL_SIZE
+N_EXPERIMENTS = 10000
 
-true_treatment_mean = true_control_mean * (1 + true_relative_diff)
-control_params = {"n": shape, "p": shape / (shape + true_control_mean)}
-treatment_params = {"n": shape, "p": shape / (shape + true_treatment_mean)}
-
-rng = np.random.default_rng(seed)
-sample_distr = rng.negative_binomial
-size_distr = rng.poisson
+SHAPE = 0.2
+CONTROL_PARAMS = {
+    'n': SHAPE,
+    'p': SHAPE / (SHAPE + TRUE_CONTROL_MEAN),
+}
+TREATMENT_PARAMS = {
+    'n': SHAPE,
+    'p': SHAPE / (SHAPE + TRUE_TREATMENT_MEAN),
+}
 
 
 def calc_experiment(
@@ -32,8 +35,14 @@ def calc_experiment(
     sample_distr: Callable,
     size_distr: Callable,
 ) -> dict[str, float]:
-    control = sample_distr(**control_params, size=size_distr(control_size))
-    treatment = sample_distr(**treatment_params, size=size_distr(treatment_size))
+    control = sample_distr(
+        **control_params,
+        size=size_distr(control_size),
+    )
+    treatment = sample_distr(
+        **treatment_params,
+        size=size_distr(treatment_size),
+    )
 
     mean1 = np.mean(treatment)
     mean2 = np.mean(control)
@@ -45,24 +54,26 @@ def calc_experiment(
     vn2 = np.var(control, ddof=1) / n2
     rel_diff = mean1 / mean2 - 1
 
-    abs_pvalue = stats.ttest_ind(treatment, control, equal_var="False").pvalue
-    abs_t = stats.t((vn1 + vn2) ** 2 / (vn1**2 / (n1 - 1) + vn2**2 / (n2 - 1)))
+    naive_ppf = stats.t.ppf(
+        q=0.975,
+        df=(vn1 + vn2)**2 / (vn1**2 / (n1 - 1) + vn2**2 / (n2 - 1)),
+    )
     naive_lower, naive_upper = (
-        rel_diff + abs_t.ppf([0.025, 0.975]) * np.sqrt(vn1 + vn2) / mean2
+        rel_diff
+        + np.array([-naive_ppf, naive_ppf]) * np.sqrt(vn1 + vn2) / mean2
     )
 
-    delta_norm = stats.norm(
+    delta_lower, delta_upper = stats.norm.ppf(
+        q=[0.025, 0.975],
         loc=rel_diff,
         scale=np.sqrt((vn1 + vn2 * mean1_sq / mean2_sq) / mean2_sq),
     )
-    delta_pvalue = 2 * min(delta_norm.cdf(0), delta_norm.sf(0))
-    delta_lower, delta_upper = delta_norm.ppf([0.025, 0.975])
 
     cv1_sq = vn1 / mean1_sq
     cv2_sq = vn2 / mean2_sq
     fieller_ppf = stats.t.ppf(
-        0.975,
-        (cv1_sq + cv2_sq) ** 2 / (cv1_sq**2 / (n1 - 1) + cv2_sq**2 / (n2 - 1)),
+        q=0.975,
+        df=(cv1_sq + cv2_sq)**2 / (cv1_sq**2 / (n1 - 1) + cv2_sq**2 / (n2 - 1)),
     )
     fieller_ppf_sq = fieller_ppf * fieller_ppf
     fieller_lower, fieller_upper = (
@@ -76,130 +87,119 @@ def calc_experiment(
     ) - 1
 
     return {
-        "mean1": mean1,
-        "mean2": mean2,
-        "rel_diff": rel_diff,
-        "abs_pvalue": abs_pvalue,
-        "naive_lower": naive_lower,
-        "naive_upper": naive_upper,
-        "delta_pvalue": delta_pvalue,
-        "delta_lower": delta_lower,
-        "delta_upper": delta_upper,
-        "fieller_lower": fieller_lower,
-        "fieller_upper": fieller_upper,
+        'naive_lower': naive_lower,
+        'naive_upper': naive_upper,
+        'delta_lower': delta_lower,
+        'delta_upper': delta_upper,
+        'fieller_lower': fieller_lower,
+        'fieller_upper': fieller_upper,
     }
 
 
-def null_rejected(x: pd.Series) -> dict[str, float]:
-    lower, upper = stats.binomtest(k=np.sum(x), n=len(x), p=0.05).proportion_ci(
-        method="wilsoncc"
+def null_rejected(x: ArrayLike) -> dict[str, float]:
+    lower, upper = (
+        stats.binomtest(k=np.sum(x), n=len(x), p=0.05)
+        .proportion_ci(method='wilsoncc')
     )
-    return {"mean": np.mean(x), "conf_int": f"({round(lower, 4)}, {round(upper, 4)})"}
+
+    return {
+        'mean': np.mean(x),
+        'conf_int': f'({round(lower, 4)}, {round(upper, 4)})',
+    }
 
 
 def true_param_location(
-    lower: pd.Series, upper: pd.Series, true_param_value: float
+    lower: ArrayLike,
+    upper: ArrayLike,
+    true_param_value: float,
 ) -> dict[str, float]:
-    x = lower.lt(true_param_value) & upper.gt(true_param_value)
-    x_lower, x_upper = stats.binomtest(k=np.sum(x), n=len(x), p=0.05).proportion_ci(
-        method="wilsoncc"
+    x = (lower < true_param_value) & (true_param_value < upper)
+    x_lower, x_upper = (
+        stats.binomtest(k=np.sum(x), n=len(x), p=0.05)
+        .proportion_ci(method='wilsoncc')
     )
+
     return {
-        # "conf_level": (lower.lt(true_param_value) & upper.gt(true_param_value)).mean(),
-        "conf_level": x.mean(),
-        "conf_level_ci": f"({round(x_lower, 4)}, {round(x_upper, 4)})",
-        "quantiles": f"({lower.ge(true_param_value).mean()}, {upper.gt(true_param_value).mean()})",
+        'conf_level': x.mean(),
+        'conf_level_ci': f'({round(x_lower, 4)}, {round(x_upper, 4)})',
+        'quantiles': (
+            f'({np.mean(true_param_value <= lower)}'
+            f', {np.mean(true_param_value < upper)})'
+        ),
     }
 
 
-aa_data = pd.DataFrame(
-    calc_experiment(
-        control_params=control_params,
-        treatment_params=control_params,
-        control_size=control_size,
-        treatment_size=treatment_size,
-        sample_distr=sample_distr,
-        size_distr=size_distr,
+def main():
+    rng = np.random.default_rng(SEED)
+
+    aa_data = pd.DataFrame(
+        calc_experiment(
+            control_params=CONTROL_PARAMS,
+            treatment_params=CONTROL_PARAMS,
+            control_size=CONTROL_SIZE,
+            treatment_size=TREATEMT_SIZE,
+            sample_distr=rng.negative_binomial,
+            size_distr=rng.poisson,
+        )
+        for i in tqdm(range(N_EXPERIMENTS))
     )
-    for i in tqdm(range(n_experiments))
-)
 
-
-display(
-    pd.DataFrame(
-        {
-            pvalue_col: null_rejected(aa_data[pvalue_col].le(0.05))
-            for pvalue_col in ["abs_pvalue", "delta_pvalue"]
-        }
-    ).transpose()
-)
-
-display(
-    pd.DataFrame(
-        {
-            prefix: null_rejected(
-                aa_data[f"{prefix}_lower"].ge(0) | aa_data[f"{prefix}_upper"].le(0)
+    print(
+        pd.DataFrame({
+            ci: null_rejected(
+                aa_data[f'{ci}_lower'].ge(0) | aa_data[f'{ci}_upper'].le(0)
             )
-            for prefix in ["naive", "delta", "fieller"]
-        }
-    ).transpose()
-)
+            for ci in ['naive', 'delta', 'fieller']
+        })
+        .transpose()
+    )
 
-display(
-    pd.DataFrame(
-        {
-            prefix: true_param_location(
-                lower=aa_data[f"{prefix}_lower"],
-                upper=aa_data[f"{prefix}_upper"],
+    print(
+        pd.DataFrame({
+            ci: true_param_location(
+                lower=aa_data[f'{ci}_lower'],
+                upper=aa_data[f'{ci}_upper'],
                 true_param_value=0,
             )
-            for prefix in ["naive", "delta", "fieller"]
-        }
-    ).transpose()
-)
-
-
-ab_data = pd.DataFrame(
-    calc_experiment(
-        control_params=control_params,
-        treatment_params=treatment_params,
-        control_size=control_size,
-        treatment_size=treatment_size,
-        sample_distr=sample_distr,
-        size_distr=size_distr,
+            for ci in ['naive', 'delta', 'fieller']
+        })
+        .transpose()
     )
-    for i in tqdm(range(n_experiments))
-)
 
-display(
-    pd.DataFrame(
-        {
-            pvalue_col: null_rejected(ab_data[pvalue_col].le(0.05))
-            for pvalue_col in ["abs_pvalue", "delta_pvalue"]
-        }
-    ).transpose()
-)
 
-display(
-    pd.DataFrame(
-        {
-            prefix: null_rejected(
-                ab_data[f"{prefix}_lower"].ge(0) | ab_data[f"{prefix}_upper"].le(0)
+    ab_data = pd.DataFrame(
+        calc_experiment(
+            control_params=CONTROL_PARAMS,
+            treatment_params=TREATMENT_PARAMS,
+            control_size=CONTROL_SIZE,
+            treatment_size=TREATEMT_SIZE,
+            sample_distr=rng.negative_binomial,
+            size_distr=rng.poisson,
+        )
+        for i in tqdm(range(N_EXPERIMENTS))
+    )
+
+    print(
+        pd.DataFrame({
+            ci: null_rejected(
+                ab_data[f'{ci}_lower'].ge(0) | aa_data[f'{ci}_upper'].le(0)
             )
-            for prefix in ["naive", "delta", "fieller"]
-        }
-    ).transpose()
-)
+            for ci in ['naive', 'delta', 'fieller']
+        })
+        .transpose()
+    )
 
-display(
-    pd.DataFrame(
-        {
-            prefix: true_param_location(
-                lower=ab_data[f"{prefix}_lower"],
-                upper=ab_data[f"{prefix}_upper"],
-                true_param_value=true_relative_diff,
+    print(
+        pd.DataFrame({
+            ci: true_param_location(
+                lower=ab_data[f'{ci}_lower'],
+                upper=ab_data[f'{ci}_upper'],
+                true_param_value=TRUE_RELATIVE_DIFF,
             )
-            for prefix in ["naive", "delta", "fieller"]
-        }
-    ).transpose()
-)
+            for ci in ['naive', 'delta', 'fieller']
+        })
+        .transpose()
+    )
+
+if __name__ == '__main__':
+    main()
